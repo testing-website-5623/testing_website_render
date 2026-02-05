7291f4b8f91a0bc2ce619350ede8fe43d669f56c
AL AMEEN <alameenallu7306@gmail.com>
2026-02-05 17:58:00 +0000

    again

--- app.py
+++ app.py
@@ -1,27 +1,13 @@
-from flask import Flask, render_template, request, redirect, session
+from cs50 import SQL
+from flask import Flask, render_templates, redirect, request, session
+from flask-session import Session
 
 app = Flask(__name__)
-app.session_key = "secret"
+
+db = SQL("sqlite:///users.db")
+
+app.config["SESSION_PERMENENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
-def home():
-    return render_template("layout.html")
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.session.method == "POST":
-        sessionp["user"] = request.form["username"]
-        return render_template("/profile")
-    
-    return render_template("login.html")
-
-@app.route("/profile")
-def profile():
-    if "user" not in session:
-        return redirect("/login")
-    return render_template("profile.html")
-
-@app.route("/logout")
-def logout():
-    session.pop("user", None)
-    return redirect("/")
