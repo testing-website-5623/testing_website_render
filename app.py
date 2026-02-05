fadfc4ceb663a3730dbf0576c2d8f309a18ac425
AL AMEEN <alameenallu7306@gmail.com>
2026-02-05 18:23:36 +0000

    again

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from cs50 import SQL
-from flask import Flask, render_templates, redirect, request, session
-from flask-session import Session
+from flask import Flask, render_template, redirect, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import login_required
 
 app = Flask(__name__)
 
@@ -11,3 +13,58 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 @app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+
+    if request.method == "POST":
+
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return render_template("error.html")
+        
+        session["user_id"] = rows[0]["id"]
+
+        return redirect("/")
+
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        rows = db.execute("SELECT * FROM users WHERE username=?", request.form.get("username"))
+
+        if len(rows) == 0:
+
+            new_id = db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", request.form.get(
+                "username"), generate_password_hash(request.form.get("password")))
+
+            session["user_id"] = new_id
+
+            return redirect("/")
+
+        else:
+
+            return render_template("error.html")
+
+    return render_template("register.html")
