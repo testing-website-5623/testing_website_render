c5dee8320252273c845a97a3f298cd908edf64b4
AL AMEEN <alameenallu7306@gmail.com>
2026-02-05 18:27:19 +0000

    nvb

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 db = SQL("sqlite:///users.db")
 
-app.config["SESSION_PERMENENT"] = False
+app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
